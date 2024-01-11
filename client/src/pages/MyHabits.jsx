1fb38b5948f2478e0be63e1c670d25e5b932708b
Clement <clement.shum128@gmail.com>
2024-01-10 21:35:34 -0500

    date changer

--- client/src/pages/MyHabits.jsx
+++ client/src/pages/MyHabits.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import { DatePicker } from "@mui/x-date-pickers";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 function MyHabits() {
+  const [date, setDate] = useState(new Date());
+
+  const decreaseDate = () => {
+    setDate((prev) => {
+      // Create a new Date instance to avoid mutating the original date
+      const newDate = new Date(prev);
+      // Decrease the date by one day
+      newDate.setDate(newDate.getDate() - 1);
+      return newDate;
+    });
+  };
+
+  const increaseDate = () => {
+    setDate((prev) => {
+      // Create a new Date instance to avoid mutating the original date
+      const newDate = new Date(prev);
+      // Increase the date by one day
+      newDate.setDate(newDate.getDate() + 1);
+      return newDate;
+    });
+  };
+
   return (
     <div>
       <Header />
-      <p className="content-container content-header-margin">My Habits</p>
+      <div className="content-container content-header-margin">
+        <div className="flex flex-row justify-center items-center gap-x-2">
+          <Button
+            onClick={decreaseDate}
+            className="bg-inherit text-black hover:bg-inherit"
+          >
+            <ArrowLeft />
+          </Button>
+          <DatePicker value={date} onChange={(newDate) => setDate(newDate)} />
+          <Button
+            onClick={increaseDate}
+            className="bg-inherit text-black hover:bg-inherit"
+          >
+            <ArrowRight />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
