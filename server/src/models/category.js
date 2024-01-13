c3ee6ca56bb10c1c191eb19edfd0877fe0dd3272
Clement <clement.shum128@gmail.com>
2024-01-12 21:07:05 -0500

    category model

---
+++ server/src/models/category.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  color: {
+    type: String,
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
