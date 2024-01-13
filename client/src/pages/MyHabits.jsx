19a20c8f3c91f47fe3410a829b84a3a17c23c877
Clement <clement.shum128@gmail.com>
2024-01-12 21:06:56 -0500

    my habit page update

--- client/src/pages/MyHabits.jsx
+++ client/src/pages/MyHabits.jsx
@@ -1,51 +1,19 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import { DatePicker } from "@mui/x-date-pickers";
-import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import DateFlipper from "../components/DateFlipper";
+import AddHabit from "../components/AddHabit";
+import AddCategory from "../components/AddCategory";
 
 function MyHabits() {
-  const [date, setDate] = useState(new Date());
-
-  const decreaseDate = () => {
-    setDate((prev) => {
-      // Create a new Date instance to avoid mutating the original date
-      const newDate = new Date(prev);
-      // Decrease the date by one day
-      newDate.setDate(newDate.getDate() - 1);
-      return newDate;
-    });
-  };
-
-  const increaseDate = () => {
-    setDate((prev) => {
-      // Create a new Date instance to avoid mutating the original date
-      const newDate = new Date(prev);
-      // Increase the date by one day
-      newDate.setDate(newDate.getDate() + 1);
-      return newDate;
-    });
-  };
+  // const [date, setDate] = useState(new Date());
 
   return (
     <div>
       <Header />
       <div className="content-container content-header-margin">
-        <div className="flex flex-row justify-center items-center gap-x-2">
-          <Button
-            onClick={decreaseDate}
-            className="bg-inherit text-black hover:bg-inherit"
-          >
-            <ArrowLeft />
-          </Button>
-          <DatePicker value={date} onChange={(newDate) => setDate(newDate)} />
-          <Button
-            onClick={increaseDate}
-            className="bg-inherit text-black hover:bg-inherit"
-          >
-            <ArrowRight />
-          </Button>
-        </div>
+        <DateFlipper />
+        {/* <AddHabit /> */}
+        <AddCategory />
       </div>
     </div>
   );
