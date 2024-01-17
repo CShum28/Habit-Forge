129fff0c0ef0760a2e90b0e6c3efcea98eac91f4
Clement <clement.shum128@gmail.com>
2024-01-17 17:54:20 -0500

    categories Api createApi

---
+++ client/src/app/api/categories/categoriesApi.jsx
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BASE_URL,
+});
+
+export const categoriesApi = createApi({
+  baseQuery,
+  reducerPath: "categoriesApi",
+  tagTypes: ["Categories"],
+  endpoints: (builder) => ({
+    getCategoriesById: builder.query({
+      query: () => {
+        return {
+          url: "/api/get-categories",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+          credentials: "include",
+        };
+      },
+      transformResponse: async (response) => {
+        // console.log(response);
+        return response;
+      },
+    }),
+  }),
+});
+
+export const { useGetCategoriesByIdQuery } = categoriesApi;
