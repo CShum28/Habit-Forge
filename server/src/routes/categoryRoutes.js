324554117cae52be26d07f4fb565b5c7c337e622
Clement <clement.shum128@gmail.com>
2024-01-22 18:53:54 -0500

    put category route

--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -15,4 +15,20 @@ router.delete("/:id", userAuth, (req, res) => {
     );
 });
 
+router.put("/:id", userAuth, (req, res) => {
+  const { id } = req.params; // grab id for the search
+  const { category, color } = req.body;
+
+  const filter = { _id: id };
+  const update = { name: category, color };
+
+  console.log(update);
+
+  Categories.findOneAndUpdate(filter, update, { new: true })
+    .then(() => res.status(200).json({ message: "Category updated" }))
+    .catch((err) =>
+      res.status(500).json({ message: "Error updating category", error: err })
+    );
+});
+
 module.exports = router;
