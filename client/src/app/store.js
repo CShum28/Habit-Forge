2fb48d6e041fa8ffaeb540b35195ad13f23c2575
Clement <clement.shum128@gmail.com>
2024-01-24 22:41:55 -0500

    update store to include habitsApi

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,15 +3,19 @@ import authSliceReducer from "../feature/auth/authSlice";
 import dateSliceReducer from "../feature/date/dateSlice";
 
 import { categoriesApi } from "./api/categories/categoriesApi";
+import { habitsApi } from "./api/habits/habitsApi";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
     date: dateSliceReducer,
     [categoriesApi.reducerPath]: categoriesApi.reducer,
+    [habitsApi.reducerPath]: habitsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(categoriesApi.middleware);
+    return getDefaultMiddleware()
+      .concat(categoriesApi.middleware)
+      .concat(habitsApi.middleware);
   },
 });
 
