efcb03e0717fd2351d9a1eb22df1d9b66fdd1210
Clement <clement.shum128@gmail.com>
2024-01-25 19:58:26 -0500

    update habits to display on date

--- client/src/components/Habit.jsx
+++ client/src/components/Habit.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Pencil } from "lucide-react";
+import { Trash2 } from "lucide-react";
+
+import { useSelector } from "react-redux";
 
 function Habit({ habit }) {
   // State to track if the checkbox is checked
   const [isChecked, setIsChecked] = useState(habit.completed);
+
+  //Grab current date
+  const dateString = useSelector((state) => state.date.value);
+  // convert ISO string into date
+  const date = new Date(dateString);
+
+  // Gives you what day it is (ex. Monday, Tuesday, etc.)
+  const dayOfWeek = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+  }).format(date);
+
+  // Checks your habit is on the right day
+  const isHabitDueToday = habit.days.includes(dayOfWeek);
 
   // Handler to update the checkbox state and potentially update the backend
   const handleCheckboxChange = () => {
@@ -22,19 +39,25 @@ function Habit({ habit }) {
       .catch((err) => console.log(err));
   };
 
-  useEffect(() => {
-    console.log(`${habit.name} is currently ${isChecked ? "true" : "false"}`);
-  }, [isChecked]);
+  //   useEffect(() => {
+  //     console.log(`${habit.name} is currently ${isChecked ? "true" : "false"}`);
+  //   }, [isChecked]);
 
   return (
-    <div className="flex flex-row items-center gap-x-2">
-      <input
-        type="checkbox"
-        onChange={handleCheckboxChange}
-        checked={isChecked}
-      />
-      <label>{habit.name}</label>
-    </div>
+    <>
+      {isHabitDueToday && (
+        <div className="flex flex-row items-center gap-x-2 my-3">
+          <input
+            type="checkbox"
+            onChange={handleCheckboxChange}
+            checked={isChecked}
+          />
+          <label>{habit.name}</label>
+          <Pencil />
+          <Trash2 />
+        </div>
+      )}
+    </>
   );
 }
 
