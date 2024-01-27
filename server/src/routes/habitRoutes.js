3ce3df509f97da0022a24bb5523a64be61606025
Clement <clement.shum128@gmail.com>
2024-01-27 17:45:15 -0500

    add delete habit route

--- server/src/routes/habitRoutes.js
+++ server/src/routes/habitRoutes.js
@@ -24,13 +24,13 @@ router.post("/", userAuth, (req, res) => {
     });
 });
 
-router.patch("/:id/toggleCheck", userAuth, (req, res) => {
-  const habitId = req.params.id;
+// router.patch("/:id/toggleCheck", userAuth, (req, res) => {
+//   const habitId = req.params.id;
 
-  console.log(habitId);
-  res.send(habitId);
-  // UPDATE THE PATCH ROUTE AFTER SUBMITTING A PATCH
-});
+//   console.log(habitId);
+//   res.send(habitId);
+//   // UPDATE THE PATCH ROUTE AFTER SUBMITTING A PATCH
+// });
 
 // Get habits for category
 router.get("/", userAuth, (req, res) => {
@@ -38,12 +38,22 @@ router.get("/", userAuth, (req, res) => {
 
   ToDoItem.find({ category_id: categoryId })
     .then((habits) => {
-      console.log(habits);
       res.status(200).json(habits);
     })
     .catch((err) => {
       res.status(400).json({ message: err.message });
     });
+});
+
+// // Delete habit path
+router.delete("/:id", userAuth, (req, res) => {
+  const { id } = req.params;
+
+  ToDoItem.deleteOne({ _id: id })
+    .then(() => res.status(200).json({ message: "Habit deleted" }))
+    .catch((err) =>
+      res.status(500).json({ message: "Error deleting habit: ", err })
+    );
 });
 
 module.exports = router;
