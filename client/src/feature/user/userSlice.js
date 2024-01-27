e98b89da0d0cd2034e502b9361a8898b091f01ad
Clement <clement.shum128@gmail.com>
2024-01-27 17:44:53 -0500

    remove code

--- client/src/feature/user/userSlice.js
+++
@@ -1,2 +0,0 @@
-// whenever the frotnedn is running, use a useEffect to check if there is a token
-// if there is a token, send a requested to backend to verify the token - otherwise, ask user to register
