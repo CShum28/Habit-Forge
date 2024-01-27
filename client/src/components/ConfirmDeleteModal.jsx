f70067a2021dfe9913a068bde9b1382a328a4b45
Clement <clement.shum128@gmail.com>
2024-01-27 17:44:32 -0500

    add delete habit section

--- client/src/components/ConfirmDeleteModal.jsx
+++ client/src/components/ConfirmDeleteModal.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 
 import { Trash2 } from "lucide-react";
 import { useGetCategoriesByIdQuery } from "../app/api/categories/categoriesApi";
+import { useGetHabitsByIdQuery } from "../app/api/habits/habitsApi";
+import { skipToken } from "@reduxjs/toolkit/query/react";
 
 import {
   Dialog,
@@ -14,23 +16,46 @@ import {
 
 import { Button } from "@/components/ui/button";
 
-function ConfirmDeleteModal({ category }) {
+function ConfirmDeleteModal({ category, habit, categoryId }) {
   // refetch the categories and rerenders the list being displayed
-  const { refetch } = useGetCategoriesByIdQuery();
+  const { refetch: categoryRefetch } = useGetCategoriesByIdQuery();
+  // refetch the categories and rerenders the list being displayed
+  // use skipToken to not fetch if categoryId does not exist (null or undefined)
+  const { refetch: habitRefetch } = useGetHabitsByIdQuery(
+    categoryId ?? skipToken
+  );
 
   const handleConfirmDelete = () => {
     console.log("confirm delete");
-    // console.log(category);
 
     const baseUrl = import.meta.env.VITE_BASE_URL;
-    axios
-      .delete(`${baseUrl}/api/category/${category._id}`, {
-        withCredentials: true, // Necessary for sending cookies over cross-domain requests
-      })
-      .then((res) => {
-        console.log(res.data);
-        refetch();
-      });
+    // handles category delete
+    if (category) {
+      axios
+        .delete(`${baseUrl}/api/category/${category._id}`, {
+          withCredentials: true, // Necessary for sending cookies over cross-domain requests
+        })
+        .then((res) => {
+          console.log(res.data);
+          categoryRefetch();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // handles habit delete
+    } else if (habit) {
+      axios
+        .delete(`${baseUrl}/api/habit/${habit._id}`, {
+          withCredentials: true, // Necessary for sending cookies over cross-domain requests
+        })
+        .then((res) => {
+          console.log(res);
+          habitRefetch();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   return (
@@ -42,7 +67,7 @@ function ConfirmDeleteModal({ category }) {
       </DialogTrigger>
       <DialogContent className="flex flex-col justify-center items-center">
         <DialogTitle>Are you sure you want to delete?</DialogTitle>
-        <div className="gap-y-4">
+        <div className="flex flex-row gap-x-3">
           <Button className="bg-red-400" onClick={handleConfirmDelete}>
             Confirm
           </Button>
