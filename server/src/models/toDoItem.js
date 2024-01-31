599fdfa641a31c564522c51ddc30996f658d787e
Clement <clement.shum128@gmail.com>
2024-01-30 22:24:31 -0500

    db update w. array for containing complete items

--- server/src/models/toDoItem.js
+++ server/src/models/toDoItem.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
+// Schema to track completion status for each day
+const completionStatusSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// ToDoItem scheuma
 const toDoItemSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -19,10 +32,7 @@ const toDoItemSchema = new mongoose.Schema({
     type: [String], // Array of days like ["Monday", "Wednesday", "Friday"]
     required: true,
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
+  completionStatus: [completionStatusSchema], //Array of completed status for each day
 });
 
 const ToDoItem = mongoose.model("ToDoItem", toDoItemSchema);
