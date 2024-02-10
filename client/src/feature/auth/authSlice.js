4be4132fdfe6d50d44e71ab33c128d6fc99a5981
Clement <clement.shum128@gmail.com>
2024-02-09 20:04:57 -0500

    update localStorage

--- client/src/feature/auth/authSlice.js
+++ client/src/feature/auth/authSlice.js
@@ -13,19 +13,27 @@ export const authSlice = createSlice({
       state.value.username = username;
       state.value.firstName = firstName;
       state.value.lastName = lastName;
-      // state.value.token = token;
       localStorage.setItem("username", username);
+      localStorage.setItem("firstName", firstName);
+      localStorage.setItem("lastName", lastName);
     },
     logOut: (state, action) => {
       state.value.username = null;
       state.value.firstName = null;
       state.value.lastName = null;
-      // state.value.token = null;
       localStorage.removeItem("username");
+      localStorage.removeItem("firstName");
+      localStorage.removeItem("lastName");
     },
     // Reducer to initialize the state from localStorage
-    initializeAuth: (state, action) => {
-      state.value.username = action.payload.username;
+    initializeAuth: (state) => {
+      const username = localStorage.getItem("username");
+      const firstName = localStorage.getItem("firstName");
+      const lastName = localStorage.getItem("lastName");
+
+      state.value.username = username;
+      state.value.firstName = firstName || state.value.firstName; // Don't overwrite if null/undefined
+      state.value.lastName = lastName || state.value.lastName; // Don't overwrite if null/undefined
     },
   },
 });
@@ -34,8 +42,3 @@ export const authSlice = createSlice({
 export const { setCredentials, logOut, initializeAuth } = authSlice.actions;
 
 export default authSlice.reducer;
-
-// new code
-
-// export const selectCurrentUser = (state) => state.auth.value.username;
-// export const selectCurrentToken = (state) => state.auth.value.token;
