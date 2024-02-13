709895c2a7cf98914adaac86c58fccdb07b28d97
Clement <clement.shum128@gmail.com>
2024-02-13 18:46:20 -0500

    update weekly review submission

--- server/src/routes/weeklyReviewRoutes.js
+++ server/src/routes/weeklyReviewRoutes.js
@@ -7,47 +7,39 @@ const toDoItem = require("../models/toDoItem");
 router.post("/", userAuth, (req, res) => {
   const { monday, sunday } = req.body;
 
+  // Using monday as the first day of the week
   let dayOfWeek = new Date(monday);
-  //   const start = new Date(monday);
-  //   const endDate = new Date(sunday);
-  //   const end = endDate.setDate(endDate.getDate() + 1);
-  //   let loop = new Date(start);
-  //   while (loop <= end) {
-  //     console.log(loop);
-  //     let newDate = loop.setDate(loop.getDate() + 1);
-  //     loop = new Date(newDate);
-  //   }
 
   // The user object here is attached by the userAuth middleware
   // after verifying the JWT token and fetching the user from the database.
   const user = req.user;
 
-  let weeklyData = {
-    monday: {
+  const weeklyData = {
+    Monday: {
       completed: 0,
       total: 0,
     },
-    tuesday: {
+    Tuesday: {
       completed: 0,
       total: 0,
     },
-    wednesday: {
+    Wednesday: {
       completed: 0,
       total: 0,
     },
-    thurday: {
+    Thurday: {
       completed: 0,
       total: 0,
     },
-    friday: {
+    Friday: {
       completed: 0,
       total: 0,
     },
-    saturday: {
+    Saturday: {
       completed: 0,
       total: 0,
     },
-    sunday: {
+    Sunday: {
       completed: 0,
       total: 0,
     },
@@ -57,9 +49,30 @@ router.post("/", userAuth, (req, res) => {
     for (const day in weeklyData) {
       // Grabbing the specific day of the week to check and compare with DB
       let formattedDate = dayOfWeek.toISOString().split("T")[0];
+
+      results.map((habit) => {
+        // Grab array of days from the completionStatus
+        const completedDatesArr = habit.completionStatus.map(
+          (completedDate) => {
+            return completedDate.date.toISOString().split("T")[0];
+          }
+        );
+
+        // Checks and sums up the total completed
+        if (completedDatesArr.includes(formattedDate)) {
+          weeklyData[day].completed++;
+        }
+
+        // Checks and sums up the total for the day
+        if (habit.days.includes(day)) {
+          weeklyData[day].total++;
+        }
+      });
+      // Move on to the next day
       dayOfWeek.setDate(dayOfWeek.getDate() + 1);
-      console.log(weeklyData[day].total);
     }
+
+    console.log(weeklyData);
   });
 });
 
