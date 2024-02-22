bc422166c415308d102b7930d127bc8ab9018f38
Clement <clement.shum128@gmail.com>
2024-02-22 18:42:34 -0500

    update dateSlice

--- client/src/feature/date/dateSlice.js
+++ client/src/feature/date/dateSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: new Date().toISOString(), // Store date as an ISO string or RTK will throw an error in console
+  value: new Date().toLocaleString(), // Store date as an ISO string or RTK will throw an error in console
 };
 
 export const dateSlice = createSlice({
@@ -11,16 +11,16 @@ export const dateSlice = createSlice({
     increaseDate: (state) => {
       const newDate = new Date(state.value);
       newDate.setDate(newDate.getDate() + 1);
-      state.value = newDate.toISOString();
+      state.value = newDate.toLocaleString();
     },
     decreaseDate: (state) => {
       const newDate = new Date(state.value);
       newDate.setDate(newDate.getDate() - 1);
-      state.value = newDate.toISOString();
+      state.value = newDate.toLocaleString();
     },
     setDate: (state, action) => {
       const newDate = new Date(action.payload);
-      state.value = newDate.toISOString();
+      state.value = newDate.toLocaleString();
     },
   },
 });
