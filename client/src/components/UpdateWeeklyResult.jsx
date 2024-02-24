d052ba63963f1b5394be4f750598f554b707e23b
Clement <clement.shum128@gmail.com>
2024-02-23 20:09:55 -0500

    updateWeeklyResults finalized

--- client/src/components/UpdateWeeklyResult.jsx
+++ client/src/components/UpdateWeeklyResult.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
-import { useGetWeeklyReviewsQuery } from "../app/api/weekly-review/weeklyReviewApi";
+import { useSelector } from "react-redux";
+import {
+  useGetWeeklyReviewsQuery,
+  useGetWeeklyReviewByDateQuery,
+} from "../app/api/weekly-review/weeklyReviewApi";
 
 function UpdateWeeklyResult({ monday, sunday }) {
   const { refetch: refetchWeeklyData } = useGetWeeklyReviewsQuery();
+
+  //Grab current date
+  const dateString = useSelector((state) => state.date.value);
+
+  // Rearrange the date to fit and match up with the date we are lookig for inside of weeklyResults database
+  const dateParts = dateString.split(",")[0].split("/");
+
+  // Ensure month and day are in two-digit format
+  const month = dateParts[0].padStart(2, "0");
+  const day = dateParts[1].padStart(2, "0");
+  const year = dateParts[2];
+
+  // Format date so its the exact same as the one in the database so we can match it up and pull the right info
+  const formattedDate = `${year}-${month}-${day}T05:00:00.000+00:00`;
+  // properDate is being used here
+  const { data: weeklyReviewByDateData } =
+    useGetWeeklyReviewByDateQuery(formattedDate);
 
   const updateWeeklyResults = () => {
     const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -14,16 +35,20 @@ function UpdateWeeklyResult({ monday, sunday }) {
       sunday: sunday.toLocaleString().split(",")[0],
     };
 
-    console.log("updated!");
-    // axios
-    //   .post(`${baseUrl}/api/weekly-review`, data, { withCredentials: true })
-    //   .then((res) => {
-    //     console.log(res);
-    //     refetchWeeklyData();
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    // ID of the weekly review that needs to be updated
+    const weeklyReviewId = weeklyReviewByDateData[0]._id;
+
+    axios
+      .put(`${baseUrl}/api/weekly-review/${weeklyReviewId}`, data, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+        refetchWeeklyData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <div className="mt-4">
