ac02a7159bb7fc109c39c1f7f70ab5d38750f245
Clement <clement.shum128@gmail.com>
2024-03-01 19:08:16 -0500

    colors

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -17,12 +17,23 @@ function Category({ category }) {
   // convert ISO string into date
   const date = new Date(dateString);
 
+  // Rearrange the date to fit and match up with the date we are lookig for inside of weeklyResults database
+  const dateParts = dateString.split(",")[0].split("/");
+
+  // Ensure month and day are in two-digit format
+  const month = dateParts[0].padStart(2, "0");
+  const day = dateParts[1].padStart(2, "0");
+  const year = dateParts[2];
+
+  // Format date so its the exact same as the one in the database so we can match it up and pull the right info
+  // Use to compare and check if date within the complete section
+  const formattedDate = `${year}-${month}-${day}`;
+
   // Gives you what day it is (ex. Monday, Tuesday, etc.)
   const dayOfWeek = new Intl.DateTimeFormat("en-US", {
     weekday: "long",
   }).format(date);
 
-  // REFACTOR THIS AFTER
   // Empty array to calculate the number of ACTUAL completed habits
   let actualCompleted = 0;
   // Map through habits
@@ -30,12 +41,13 @@ function Category({ category }) {
     // console.log(habit);
     const complete = habit.completionStatus;
     // Map through habits again to count the number of completed for the day
+
     if (complete) {
       complete.map((completeInfo) => {
         // for some reason when the date gets here the timezone changes
         // NEED TO FIGURE THIS PART OUT LOOK INTO DATE SLICE AND UTC VALUE
         // Compare date and date of completed habit info
-        if (completeInfo.date.split("T")[0] === dateString.split("T")[0]) {
+        if (completeInfo.date.split("T")[0] === formattedDate) {
           actualCompleted++;
         }
       });
@@ -48,18 +60,26 @@ function Category({ category }) {
   habitsData?.map((habit) => {
     if (habit.days.includes(dayOfWeek)) {
       completeTotal.push(habit);
+      console.log("true");
     }
   });
+  console.log(completeTotal);
 
   return (
     <div>
       <div
         key={category._id}
-        className="flex flex-row justify-between border-b-4 mb-2 border-slate-400"
+        className={`flex flex-row justify-between border-b-4 mb-2 border-slate-400 px-2 py-1 rounded-t-lg ${
+          category.color === "blue"
+            ? "bg-blue-200"
+            : category.color === "red"
+            ? "bg-red-200"
+            : category.color === "green"
+            ? "bg-green-200"
+            : ""
+        }`}
       >
-        <p>
-          {category.name} - color: {category.color}
-        </p>
+        <p>{category.name}</p>
 
         <div className="flex flex-row gap-x-2">
           <AddHabitModal category={category} />
